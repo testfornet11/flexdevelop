a29ce0896128f35962b58d6102232c859de18427
testfornet11 <testfornet11@gmail.com>
2020-02-16 15:08:03 +0100

    no message

--- src/components/Main.js
+++ src/components/Main.js
@@ -84,7 +84,7 @@ class Main extends React.Component {
           <h2 className="major">Kapcsolat</h2>
           <form name="contact" method="POST" data-netlify="true">
             <div className="field half first">
-              <label htmlFor="name">Name</label>
+              <label htmlFor="name">Név</label>
               <input type="text" name="name" id="name" />
             </div>
             <div className="field half">
@@ -92,7 +92,7 @@ class Main extends React.Component {
               <input type="text" name="email" id="email" />
             </div>
             <div className="field">
-              <label htmlFor="message">Message</label>
+              <label htmlFor="message">Üzenet</label>
               <textarea name="message" id="message" rows="4"></textarea>
             </div>
             <ul className="actions">
