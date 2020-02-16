5962c84cb569c578c848a24253606c383cbcb5eb
testfornet11 <testfornet11@gmail.com>
2020-02-16 14:43:10 +0100

    no message

--- src/components/Main.js
+++ src/components/Main.js
@@ -82,7 +82,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Kapcsolat</h2>
-          <form method="post" action="#">
+          <form name="contact" method="POST" data-netlify="true">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
