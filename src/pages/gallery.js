4f74cb5e1b021f419d69918b1252b5ad022628b8
testfornet11 <testfornet11@gmail.com>
2020-06-14 11:59:11 +0200

    no message

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -3,6 +3,8 @@ import Bankgal from '../components/Bankgal'
 import Szatmargal from '../components/Szatmargal'
 import Hoszigetelesgal from '../components/Hoszigetelesgal'
 import Retkerulogal from '../components/Retkerulogal'
+import Retkozgal from '../components/Retkozgal'
+import Tomorigal from '../components/Tomorigal'
 
 export default () => (
   <div className="galleryContainer">
@@ -10,5 +12,7 @@ export default () => (
   <Hoszigetelesgal />
   <Bankgal />
   <Retkerulogal />
+  <Retkozgal />
+  <Tomorigal />
   </div>
 )
